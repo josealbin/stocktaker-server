5d0be8f485594561b8a96c2a60baddd3f2f25b44
jose albin <josealbin@192-168-68-107.tpgi.com.au>
2024-07-21 09:44:00 +1000

    stock taker server fixing

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors({
 }));
 app.use(express.json());
 
-mongoose.connect('https://stocktaker-client.vercel.app/stockTaker')
+mongoose.connect('mongodb+srv://josealbin:dars9252@stocktaker.lsqhay3.mongodb.net/stockTaker?retryWrites=true&w=majority&appName=stockTaker')
 
 
 const storage = multer.diskStorage({
