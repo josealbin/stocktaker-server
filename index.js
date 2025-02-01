f22e5c91993c268c99e356123bc916be174f2612
jose albin <josealbin@192-168-68-109.tpgi.com.au>
2025-02-01 15:01:36 +1000

    changed to original

--- index.js
+++ index.js
@@ -13,13 +13,9 @@ import './config/db.js'
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin: ["https://stocktaker-client.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-    //allowedHeaders: ["Content-Type", "Authorization"]
-}));
 dotenv.config({path: "./config/.env"})
+
+app.use(cors());
 
 //mongoose.connect('mongodb://127.0.0.1:27017/stocklist')
 
