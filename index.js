e8650a36da9782e834aefe4f7d92a5975f3f089b
jose albin <josealbin@albins-MacBook-Pro.local>
2025-04-18 23:13:30 +1000

    admin login final changes

--- index.js
+++ index.js
@@ -179,11 +179,11 @@ app.post('/admin-login', (req, res) => {
     const adminPassword = process.env.ADMIN_PASSWORD;
 
     if (email !== adminEmail) {
-      return res.status(401).json({ message: 'Invalid admin email' });
+      return res.status(401).json({ message: 'Invalid admin email!' });
     }
   
     if (password !== adminPassword) {
-      return res.status(401).json({ message: 'Incorrect admin password' });
+      return res.status(401).json({ message: 'Incorrect admin password!' });
     }
   
     const token = jwt.sign({ email, role: 'admin' }, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
